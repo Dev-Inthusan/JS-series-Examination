a01fbc01f6402ee36fba546549ecb7053f09e9f3
UKI04 <in2cr7@gmail.com>
2023-10-26 10:46:46 +0530

    JS exam

---
+++ day1.js
@@ -0,0 +1,95 @@
+// 1
+let obj = [];
+console.log(obj)
+
+// 2
+let person = {
+    name:  "inthu",
+    age :20,
+    introduceYou:'Hello'
+    
+}
+
+// 3
+let greetstudent = "Cristiano";
+console.log(greetstudent);
+
+// 4
+let goat = {
+    no1:"cristiano",
+    no2:"leo",
+    no3:"neymar"
+}
+console.log(goat.no2);
+console.log(goat["no1"]);
+
+
+
+// 5
+var email = "john@example.com";
+console.log(email)
+
+
+// 6
+delete names["name1"]
+console.log(names);
+
+
+
+// 7
+
+
+
+// 8
+let student = "Ronaldo";
+let course = "Accounting";
+console.log(student,course)
+
+
+// 9
+
+
+
+
+// 10
+  let movie = {
+    title:'The lion',
+    author:"james",
+    year:2001,
+  };
+  console.log
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
